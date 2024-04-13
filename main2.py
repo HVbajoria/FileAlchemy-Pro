52c9a7d7daeabfeb552ffb341dfd68af90c428e6
Harshavardhan Bajoria <62978274+HVbajoria@users.noreply.github.com>
2024-04-13 20:48:04 +0000

    v 2.0

--- main2.py
+++ main2.py
@@ -4,6 +4,16 @@ import streamlit as st
 import shutil
 import textract
 
+# Function that removes extra newlines and spaces from the extracted text and trim each line
+def remover(text):
+    # remove the extra newlines
+    text = text.replace(b'\n\n', b'\n')
+    # remove extra spaces
+    text = text.replace(b'  ', b' ')
+    # trim each line
+    text = b'\n'.join([line.strip() for line in text.split(b'\n') if line.strip()])
+    return text
+
 def convert_doc_to_txt(input_dir, output_dir):
     # Check if the output directory exists, create it if not
     if not os.path.exists(output_dir):
@@ -14,20 +24,104 @@ def convert_doc_to_txt(input_dir, output_dir):
 
     # Iterate over the files and convert .doc and .docx files to .txt
     for file in files:
+        text=""
+        if file.endswith('.txt'):
+            # For .txt files, read the content and remove extra newlines and spaces
+            txt_path = os.path.join(input_dir, file)
+            with open(txt_path, 'rb') as txt_file:
+                text = txt_file.read()
+            text = remover(text)
+            # remove this file
+            os.remove(txt_path)
         if file.endswith(('.doc', '.docx')):
             # For .doc and .docx files, use textract library to extract text
             doc_path = os.path.join(input_dir, file)
             text = textract.process(doc_path, encoding='utf-8')
-
-            # Create the output .txt file path
-            txt_filename = os.path.splitext(file)[0] + '.txt'
-            txt_path = os.path.join(output_dir, txt_filename)
+            text = remover(text)
+        # Create the output .txt file path
+        txt_filename = os.path.splitext(file)[0].lower()
+        txt_filename = txt_filename.replace(" ", "")
+        print(txt_filename)
+        if txt_filename == 'basic1':
+            txt_filename = 'Basic_input_1'
+        if txt_filename == 'basic2':
+            txt_filename = 'Basic_input_2'
+        if txt_filename == 'basic3':
+            txt_filename = 'Basic_input_3'
+        if txt_filename == 'basic4':
+            txt_filename = 'Basic_input_4'
+        if txt_filename == 'basic1_output':
+            txt_filename = 'Basic_output_1'
+        if txt_filename == 'basic2_output':
+            txt_filename = 'Basic_output_2'
+        if txt_filename == 'basic3_output':
+            txt_filename = 'Basic_output_3'
+        if txt_filename == 'basic4_output':
+            txt_filename = 'Basic_output_4'
+        if txt_filename == 'boundary1':
+            txt_filename = 'Boundary_input_1'
+        if txt_filename == 'boundary2':
+            txt_filename = 'Boundary_input_2'
+        if txt_filename == 'boundary1_output':
+            txt_filename = 'Boundary_output_1'
+        if txt_filename == 'boundary2_output':
+            txt_filename = 'Boundary_output_2'
+        if txt_filename == 'corner1':
+            txt_filename = 'Corner_input_1'
+        if txt_filename == 'corner2':
+            txt_filename = 'Corner_input_2'
+        if txt_filename == 'corner1_output':
+            txt_filename = 'Corner_output_1'
+        if txt_filename == 'corner2_output':
+            txt_filename = 'Corner_output_2'
+        if txt_filename == 'edge1':
+            txt_filename = 'Edge_input_1'
+        if txt_filename == 'edge2':
+            txt_filename = 'Edge_input_2'
+        if txt_filename == 'edge1_output':
+            txt_filename = 'Edge_output_1'
+        if txt_filename == 'edge2_output':
+            txt_filename = 'Edge_output_2'
+        if txt_filename == 'sample1':
+            txt_filename = 'Sample_input_1'
+        if txt_filename == 'sample2':
+            txt_filename = 'Sample_input_2'
+        if txt_filename == 'sample1_output':
+            txt_filename = 'Sample_output_1'
+        if txt_filename == 'sample2_output':
+            txt_filename = 'Sample_output_2'
+        if txt_filename == 'boundary1_solution':
+            txt_filename = 'Boundary_output_1'
+        if txt_filename == 'boundary2_solution':
+            txt_filename = 'Boundary_output_2'
+        if txt_filename == 'corner1_solution':
+            txt_filename = 'Corner_output_1'
+        if txt_filename == 'corner2_solution':
+            txt_filename = 'Corner_output_2'
+        if txt_filename == 'edge1_solution':
+            txt_filename = 'Edge_output_1'
+        if txt_filename == 'edge2_solution':
+            txt_filename = 'Edge_output_2'
+        if txt_filename == 'sample1_solution':
+            txt_filename = 'Sample_output_1'
+        if txt_filename == 'sample2_solution':
+            txt_filename = 'Sample_output_2'
+        if txt_filename == 'basic1_solution':
+            txt_filename = 'Basic_output_1'
+        if txt_filename == 'basic2_solution':
+            txt_filename = 'Basic_output_2'
+        if txt_filename == 'basic3_solution':
+            txt_filename = 'Basic_output_3'
+        if txt_filename == 'basic4_solution':
+            txt_filename = 'Basic_output_4'
+        txt_filename = txt_filename + ".txt"
+        txt_path = os.path.join(output_dir, txt_filename)
 
             # Save the extracted text as .txt
-            with open(txt_path, 'wb') as txt_file:
-                txt_file.write(text)
+        with open(txt_path, 'wb') as txt_file:
+            txt_file.write(text)
 
-            print(f"Converted {file} to {txt_filename}")
+        print(f"Converted {file} to {txt_filename}")
 
 def process_zip_files(zip_file):
     # Create a directory to extract the zip contents
@@ -58,10 +152,45 @@ def process_zip_files(zip_file):
 
 def main():
     st.set_page_config(
-        page_title="FileAlchemy",
+        page_title="FileAlchemy Pro",
         page_icon="📑",
     )
-    st.title("FileAlchemy")
+    
+
+    def gradient_text(text, color1, color2):
+        gradient_css = f"""
+        background: -webkit-linear-gradient(left, {color1}, {color2});
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-weight: bold;
+        font-size: 36px;
+        """
+        return f'<span style="{gradient_css}">{text}</span>'
+
+    def gradient(text, color1, color2):
+        gradient_css = f"""
+        background: -webkit-linear-gradient(left, {color2}, {color1});
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-weight: bold;
+        font-size: 18px;
+        """
+        return f'<span style="{gradient_css}">{text}</span>'
+
+    color1 = "#FA5C3B"
+    color2 = "#2B45D5"
+    text = "FileAlchemy Pro"
+  
+    left_co, cent_co,last_co = st.columns(3)
+    with cent_co:
+        st.image("logo.png", width=140)
+
+    styled_text = gradient_text(text, color1, color2)
+    st.write(f"<div style='text-align: center;'>{styled_text}</div>", unsafe_allow_html=True)
+
+    text="Unleashing Intelligent Solutions for a Smarter Tomorrow."
+    styled_text = gradient(text, color1, color2)
+    st.write(f"<div style='text-align:center;'>{styled_text}</div>",unsafe_allow_html=True)
 
     uploaded_file = st.file_uploader("Choose a zip file", type="zip")
     st.write("Note: Upload a zip file containing .doc and .docx files")
